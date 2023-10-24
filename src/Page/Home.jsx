90b17e429162e46f2a511dd77473622ebb9f24d4
OUSHIAREXE <ouchi33r@gmail.com>
2023-10-24 13:42:32 +0100

    Remove Spinner

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,43 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import AboutMe from "../components/AboutMe";
 import Contact from "../components/Contact";
 import Projects from "../components/Projects";
 import Footer from "../components/Footer";
-import "./Spinner.css";
 
 function Home() {
-  const [showSpinner, setShowSpinner] = useState(true);
 
-  useEffect(() => {
-    const spinnerTimer = setTimeout(() => {
-      setShowSpinner(false);
-    }, 4000); // 6000 milliseconds = 6 seconds
-
-    return () => {
-      clearTimeout(spinnerTimer); // Clear the timer when the component unmounts
-    };
-  }, []);
-
+  
   return (
     <>
-      {showSpinner ? (
-        <div className="spinner-container">
-          <div className="spinner"></div>
-        </div>
-      ) : (
-        <>
-          <Navbar />
-          <Hero />
-          <AboutMe />
-          <Projects />
-          <Contact />
-          <footer>
-            <Footer />
-          </footer>
-        </>
-      )}
+      <Navbar />
+      <Hero />
+      <AboutMe/>
+      <Projects/>
+      <Contact/>
+      <footer>
+        <Footer />
+      </footer>
     </>
   );
 }
