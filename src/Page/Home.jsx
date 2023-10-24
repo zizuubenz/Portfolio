5628eebc9ca6683ac19bb0a13b56f8867c3b5241
OUSHIAREXE <ouchi33r@gmail.com>
2023-10-24 13:29:33 +0100

    Add Spinner

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import AboutMe from "../components/AboutMe";
 import Contact from "../components/Contact";
 import Projects from "../components/Projects";
 import Footer from "../components/Footer";
+import "./Spinner.css";
 
 function Home() {
+  const [showSpinner, setShowSpinner] = useState(true);
+
+  useEffect(() => {
+    const spinnerTimer = setTimeout(() => {
+      setShowSpinner(false);
+    }, 4000); // 6000 milliseconds = 6 seconds
+
+    return () => {
+      clearTimeout(spinnerTimer); // Clear the timer when the component unmounts
+    };
+  }, []);
+
   return (
     <>
-      <Navbar />
-      <Hero />
-      <AboutMe/>
-      <Projects/>
-      <Contact/>
-      <footer>
-        <Footer />
-      </footer>
+      {showSpinner ? (
+        <div className="spinner"></div>
+      ) : (
+        <>
+          <Navbar />
+          <Hero />
+          <AboutMe />
+          <Projects />
+          <Contact />
+          <footer>
+            <Footer />
+          </footer>
+        </>
+      )}
     </>
   );
 }
